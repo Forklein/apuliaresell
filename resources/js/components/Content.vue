965a1646363f8f91bb1d220df686432b4da08a3a
Forklein <giuseppepisani2@gmail.com>
2021-11-26 20:29:56 +0100

    add async await in created

--- resources/js/components/Content.vue
+++ resources/js/components/Content.vue
@@ -5,6 +5,16 @@
 <script>
 export default {
   name: "Content",
+  data() {
+    return {
+      images: [],
+      baseUri: "http://127.0.0.1:8000/api",
+    };
+  },
+  async created() {
+    const images = await axios.get(`${this.baseUri}/images`);
+    this.images = images.data.images;
+  },
 };
 </script>
 
